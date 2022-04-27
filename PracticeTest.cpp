f7dddb458259d8e41854a3191cc14c34e4a1b64d
crispy <cgutierrez57@mail.csuchico.edu>
2022-04-27 12:35:19 -0700

    tests

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -18,3 +18,31 @@ TEST(PracticeTest, smoke_test)
 {
     ASSERT_TRUE(true);
 }
+
+TEST(PracticeTest, sortDescending){
+	int i = 1;
+	int j = 2;
+	int k = 3;
+	//sortDescending(i, j, k);
+	Practice testObject;
+	testObject.sortDescending(i, j, k);
+	ASSERT_EQ(i,3);
+	ASSERT_EQ(j,2);
+	ASSERT_EQ(k, 1);
+}
+
+TEST(PracticeTest, isPalindrome){
+	char one[] = "apple";
+	char two[] = "eattae";
+	Practice testObject;
+	ASSERT_EQ(testObject.isPalindrome(one), false);
+	ASSERT_EQ(testObject.isPalindrome(two), true);
+}
+
+TEST(PracticeTest, count_starting_repeats){
+	char one[] = "aaron";
+	char two[] = "tttttttttttturtle";
+	Practice testObject;
+	ASSERT_EQ(testObject.count_starting_repeats(one), 2);
+	ASSERT_EQ(testObject.count_starting_repeats(two), 12);
+}
